4a1e7ac6edaa2b7ebb011d1aed3d7e7795fa78ba
uyjang <uyjang@inzisoft.com>
2023-11-03 13:50:55 +0900

    ios 웹뷰 호출 및 리턴 부분 완료

--- swiftUITest/swiftUITest/MyWebView.swift
+++ swiftUITest/swiftUITest/MyWebView.swift
@@ -20,8 +20,8 @@ struct MyWebView: UIViewRepresentable {
             webView.isInspectable = true
         }
         
-        // ios salesKit으로 접속했는지 파악하도록 웹에 전달
-        webView.evaluateJavaScript("navigator.userAgent"){(result, error) in
+        // iOS salesKit으로 접속했는지 파악하도록 웹에 전달
+        webView.evaluateJavaScript("navigator.userAgent") { (result, error) in
             let originUserAgent = result as! String
             let agent = originUserAgent + " saleskit_ios_app"
             webView.customUserAgent = agent
@@ -45,7 +45,9 @@ struct MyWebView: UIViewRepresentable {
         let configuration = WKWebViewConfiguration()
         let contentController = WKUserContentController()
         
-        contentController.add(self.makeCoordinator(), name: "contactsExtraction")
+        // 자바스크립트 컨트롤러 추가
+        contentController.add(self.makeCoordinator(), name: "showAlertDialog")
+        contentController.add(self.makeCoordinator(), name: "testAPI")
         
         configuration.userContentController = contentController
         return configuration
@@ -59,29 +61,66 @@ struct MyWebView: UIViewRepresentable {
             self.parent = parent
         }
         
+        // 자바스크립트 컨트롤러 추가
         func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-            if let webView = message.webView, message.name == "nativeFunction", let bodyString = message.body as? String {
-                DispatchQueue.main.async {
-                    self.showAlert(message: bodyString, webView: webView)
+            if let webView = message.webView {
+                switch message.name {
+                case "showAlertDialog":
+                    handleAlertDialog(webView: webView, bodyString: message.body as? String)
+                case "testAPI":
+                    handleTestAPI(webView: webView, bodyString: message.body as? String)
+                default:
+                    break
                 }
             }
         }
         
-        private func showAlert(message: String, webView: WKWebView) {
-            let alertController = UIAlertController(title: "Message from Web", message: message, preferredStyle: .alert)
-            alertController.addAction(UIAlertAction(title: "OK", style: .default) { _ in
-                // After alert is dismissed, send the WebViewResponse back to the web page
-                let response = WebViewResponse(ok: true, data: "[{\"name\":\"Qqq\",\"phoneNumber\":\"010-2629-4884\",\"org\":\"인지소ㅍㅌ\",\"title\":\"직위\"},{\"name\":\"ㅂㅂㅈㄷ교ㅗㅜ\",\"phoneNumber\":\"010-9090-9090\",\"org\":null,\"title\":null}]", message: nil)
-                
-                // Encode the WebViewResponse to JSON
-                if let jsonData = try? JSONEncoder().encode(response),
-                   let jsonString = String(data: jsonData, encoding: .utf8) {
-                    let jsCode = "responseNative(\(jsonString));"
-                    webView.evaluateJavaScript(jsCode, completionHandler: nil)
+        func handleAlertDialog(webView: WKWebView, bodyString: String?) {
+            if let bodyString = bodyString {
+                DispatchQueue.main.async {
+                    let alertController = UIAlertController(title: "알림", message: bodyString, preferredStyle: .alert)
+                    
+                    alertController.addAction(UIAlertAction(title: "OK", style: .default) { _ in
+                        let response = WebViewResponse(ok: true, data: "UIAlertAction", message: nil)
+                        self.executeJavaScript(response, in: webView)
+                    })
+                    
+                    if let rootViewController = UIApplication.shared.windows.first?.rootViewController {
+                        rootViewController.present(alertController, animated: true, completion: nil)
+                    }
                 }
-            })
-            if let rootViewController = UIApplication.shared.windows.first?.rootViewController {
-                rootViewController.present(alertController, animated: true, completion: nil)
+            }
+        }
+        
+        func handleTestAPI(webView: WKWebView, bodyString: String?) {
+            if let bodyString = bodyString,
+               let jsonData = bodyString.data(using: .utf8),
+               let json = try? JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any],
+               let boolValue = json["bool"] as? Bool {
+                
+                let secondValue = json["second"] as? Int ?? 0
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(secondValue)) {
+                    var messageText = "iOS 다이얼로그 예제입니다.\n"
+                    messageText += boolValue ? "참 호출됨" : "거짓 호출됨"
+                    let alertController = UIAlertController(title: "알림", message: messageText, preferredStyle: .alert)
+                    
+                    alertController.addAction(UIAlertAction(title: "OK", style: .default) { _ in
+                        let response = WebViewResponse(ok: true, data: "UIAlertAction22", message: nil)
+                        self.executeJavaScript(response, in: webView)
+                    })
+                    
+                    if let rootViewController = UIApplication.shared.windows.first?.rootViewController {
+                        rootViewController.present(alertController, animated: true, completion: nil)
+                    }
+                }
+            }
+        }
+        
+        func executeJavaScript(_ response: WebViewResponse, in webView: WKWebView) {
+            if let jsonData = try? JSONEncoder().encode(response),
+               let jsonString = String(data: jsonData, encoding: .utf8) {
+                webView.evaluateJavaScript("responseNative(\(jsonString));", completionHandler: nil)
             }
         }
         
@@ -90,7 +129,6 @@ struct MyWebView: UIViewRepresentable {
         }
     }
 }
-
 
 struct MyWebView_Previews: PreviewProvider {
     static var previews: some View {
